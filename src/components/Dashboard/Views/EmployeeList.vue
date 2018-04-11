9ddf9623f51f9bbcd81985385718bb4684e58fa2
CDT <779888925@qq.com>
2018-04-11 19:52:14 +0800

    '.'

--- src/components/Dashboard/Views/EmployeeList.vue
+++ src/components/Dashboard/Views/EmployeeList.vue
@@ -2,56 +2,26 @@
     <div class="row">
       <div class="col-md-12">
         <div class="card">
-          <paper-table :title="title" :sub-title="subTitle" :data="data" :columns="columns">
+          <basic-table :title="title" :sub-title="subTitle">
 
-          </paper-table>
+          </basic-table>
         </div>
       </div>
     </div>
 </template>
 
 <script>
-  import PaperTable from 'components/UIComponents/PaperTable.vue'
-  import axios from 'axios'
-  const tableColumns = ['工号', '姓名', '电话', '身份证']
-  const tableData = []
-
+  import BasicTable from 'components/UIComponents/BasicTable/BasicTable.vue'
+  
   export default {
     components: {
-      PaperTable
+      BasicTable
     },
     data () {
       return {
         title: '人员信息表',
-        subTitle: '院内所有人员基本信息',
-        columns: [...tableColumns],
-        data: [...tableData]
+        subTitle: '院内所有人员基本信息'
       }
-    },
-    methods: {
-      translateData: function (employees) {
-        let translatedEmployees = []
-        employees.forEach(employee => {
-          let translatedEmployee = {}
-          translatedEmployee['工号'] = employee.job_NO
-          translatedEmployee['姓名'] = employee.name
-          translatedEmployee['电话'] = employee.mobile === null ? '(空)' : employee.mobile
-          translatedEmployee['身份证'] = employee.id_NO === null ? '(空)' : employee.id_NO
-          translatedEmployees.push(translatedEmployee)
-        })
-        return translatedEmployees
-      }
-    },
-    mounted () {
-      console.log('ready to call /api/employees')
-      axios.get('/api/employees')
-           .then(
-             response => {
-               this.data = this.translateData(response.data)
-             })
-           .catch(e => {
-             console.log('error: ' + e)
-           })
     }
   }
 
